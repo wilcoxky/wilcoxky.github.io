3759bfe3e2117a5444ebdd62fcadfc8a24390267
Kyle Wilcox <wilcox32ahs@gmail.com>
2016-07-20 11:10:09 -0400

    Site updated with VFA base

--- js/main.js
+++ js/main.js
@@ -2,106 +2,128 @@ var $ = require('jquery');
 
 $(document).ready(function() {
 
-// Nav menu response
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+    // Nav menu response
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
 
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
-      }
+    menuToggle.on('click', function(e) {
+        e.preventDefault();
+        $('#js-navigation-menu').slideToggle(function() {
+            if ($('#js-navigation-menu').is(':hidden')) {
+                $('#js-navigation-menu').removeAttr('style');
+            }
+        });
     });
-  });
 
-// Smooth Anchor Scrolling
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
+    // Smooth Anchor Scrolling
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
 
-// Parallax Window
-function parallax() {
-    if( $("#js-parallax-window").length > 0 ) {
-    var plxBackground = $("#js-parallax-background");
-    var plxWindow = $("#js-parallax-window");
+    // Parallax Window
+    function parallax() {
+        if ($("#js-parallax-window").length > 0) {
+            var plxBackground = $("#js-parallax-background");
+            var plxWindow = $("#js-parallax-window");
 
-    var plxWindowTopToPageTop = $(plxWindow).offset().top;
-    var windowTopToPageTop = $(window).scrollTop();
-    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+            var plxWindowTopToPageTop = $(plxWindow).offset().top;
+            var windowTopToPageTop = $(window).scrollTop();
+            var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
 
-    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-    var windowInnerHeight = window.innerHeight;
-    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-    var plxSpeed = 0.35;
+            var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+            var windowInnerHeight = window.innerHeight;
+            var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+            var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+            var plxSpeed = 0.35;
 
-    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+            plxBackground.css('top', -(plxWindowTopToWindowTop * plxSpeed) + 'px');
+        }
     }
-  }
-  if ($("#js-parallax-window").length) {
-    parallax();
-  }
-
-$(window).scroll(function(e) {
-  if ($("#js-parallax-window").length) {
-    parallax();
-  }});
-
-
-// Modal JS
- $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
+    if ($("#js-parallax-window").length) {
+        parallax();
     }
-  });
 
-  $("#modal-2").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
+    $(window).scroll(function(e) {
+        if ($("#js-parallax-window").length) {
+            parallax();
+        }
+    });
 
-   $("#modal-3").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
+
+    // Modal JS
+    $("#modal-1").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $("#modal-2").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $("#modal-3").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
 
     $("#modal-4").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
 
-   $("#modal-5").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
+    $("#modal-5").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
 
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
+    $("#modal-8").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
 
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
+    $("#modal-6").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $("#modal-7").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+        $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+        e.stopPropagation();
+    });
 
 });
-
-
-
