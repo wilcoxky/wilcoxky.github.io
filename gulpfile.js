ae39fcb31c5241f6259ee993e15728c2d1d41bbc
Kyle Wilcox <wilcox32ahs@gmail.com>
2016-09-03 12:52:55 -0400

    Updated Content and styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+/*
+	Compile SCSS -> CSS,
+	and Min
+ */
+gulp.task('minify', function () {
+  return gulp.src('./css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
 });
