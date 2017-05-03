de4fd3093e90ac78eff9983791bdc0f6f5e612f2
Kyle Wilcox <wilcox32ahs@gmail.com>
2017-05-02 22:02:37 -0400

    Long time no pusuh 2

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var rename = require('gulp-rename');
 	Compile SCSS -> CSS,
 	and Min
   Comment for my own opush
+  saved password?
  */
 gulp.task('minify', function () {
   return gulp.src('./css/**/*.css')
